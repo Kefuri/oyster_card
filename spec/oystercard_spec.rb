8a17d316b8589b489eea0ce7a8ecf785bbbe8f65
Haydon Houghton <haydonhoughton@outlook.com>
2020-01-27 14:41:40 +0000

    Writing first tests and implementing balance increase

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,11 @@
-Describe OysterCard do
+require 'oystercard'
+describe OysterCard do
+  subject(:oyster) { described_class.new }
+  it 'returns a balance of 0' do
+    expect(oyster.balance).to eq 0
+  end
+
+  it 'increases balance with a top up value' do
+    expect { oyster.top_up(10) }.to change { oyster.balance }.from(0).to(10)
+  end
 end
